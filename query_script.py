365aa0aa0b441f4cde24bd24b068d75797de7f59
julicq <julicq@gmail.com>
2024-06-07 17:31:11 +0300

    add multilang

--- query_script.py
+++ query_script.py
@@ -2,6 +2,8 @@ from langchain_community.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.llms.ollama import Ollama
 from get_embedding_function import get_embedding_function
+from langdetect import detect
+from googletrans import Translator
 
 CHROMA_PATH = "chroma"
 
@@ -24,11 +26,27 @@ def query_rag(query_text: str):
     results = db.similarity_search_with_score(query_text, k=5)
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+
+    # Detect the language of the query text
+    query_language = detect(query_text)
+
+    # Translate the query to English if it's not already in English
+    if query_language != 'en':
+        translator = Translator()
+        query_text_en = translator.translate(query_text, src=query_language, dest='en').text
+    else:
+        query_text_en = query_text
+
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-    prompt = prompt_template.format(context=context_text, question=query_text)
+    prompt = prompt_template.format(context=context_text, question=query_text_en)
 
     model = Ollama(model="llama3:8b-instruct-q8_0")
     response_text = model.invoke(prompt)
+
+    # Translate the response back to the original language if it was translated
+    if query_language != 'en':
+        translator = Translator()
+        response_text = translator.translate(response_text, src='en', dest=query_language).text
 
     sources = [doc.metadata.get("id", None) for doc, _score in results]
     formatted_response = f"{response_text}\nSources: {sources}"
